f3444bc3e3e997f1de9773e0cb0cd873e0371523
Eddie Mallma Alvarez <emallmaa@emeal.nttdata.com>
2023-01-25 15:54:50 -0500

    initialize proyect and config feignClient step 1

---
+++ src/main/java/ves/com/villa2/Villa2Application.java
@@ -0,0 +1,13 @@
+package ves.com.villa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Villa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Villa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ves/com/villa2/Villa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ves.com.villa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Villa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
